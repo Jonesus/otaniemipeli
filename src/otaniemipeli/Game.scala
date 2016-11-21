bb8be4f95061bfd28565d2cc2aacaf5681e165c3
Juhana Saarinen <juhana.saarinen@aalto.fi>
2016-11-21 16:23:48 +0200

    Swing-kokeilu

--- src/otaniemipeli/Game.scala
+++ src/otaniemipeli/Game.scala
@@ -1,5 +1,21 @@
 package otaniemipeli
 
-class Game {
+import scala.swing._
+import java.awt.Color
 
+object Game extends SimpleSwingApplication {
+  def top = new MainFrame {
+    title = "Otaniemipeli"
+    size = new Dimension(400, 400)
+    val canvas = new Canvas
+    canvas.preferredSize = new Dimension(400, 400)
+    contents = canvas
+  }
+  
+  class Canvas extends Panel {
+    override def paintComponent(g: Graphics2D) {
+      g.setColor(Color.blue)
+      g.fillOval(0, 0, 100, 100)
+    }
+  }
 }
