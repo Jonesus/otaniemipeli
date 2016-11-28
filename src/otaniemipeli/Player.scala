0bd7be89ada99bf6d2b456a5bfcc68e0d27dbfb0
Minja <minja.axelsson@aalto.fi>
2016-11-28 15:19:23 +0200

    1st version of Player.scala

--- src/otaniemipeli/Player.scala
+++ src/otaniemipeli/Player.scala
@@ -1,5 +1,31 @@
 package otaniemipeli
+import math.{sqrt, pow}
 
 object Player {
-
+  var health = 100
+  var speed = 40
+  var graphic = "player.png"
+  var position = (0,0)
+  
+  def update() = {
+    if (speed==40){
+      position.(0)+= 1
+      position(1)+= 1
+    }
+  }
+  
+  def render() = {
+    
+  }
+  
+  def checkForCollisions(World.worldItemList) = {
+    var collisionList = []
+    for item in World.worldItemList{
+      if sqrt(pow(player.x-item.x,2) + pow(player.y-item.y,2)) < 50{
+        collisionList += item
+      }
+    }
+    return collisionList
+  }
+  
 }
