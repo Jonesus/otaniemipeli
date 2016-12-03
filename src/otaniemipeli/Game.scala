1f63b2719e52a6886a41a6f222f912363a4f1f09
Juhana Saarinen <juhana.saarinen@aalto.fi>
2016-12-03 16:19:51 +0200

    Initial commit of Game.scala.

--- src/otaniemipeli/Game.scala
+++ src/otaniemipeli/Game.scala
@@ -1,3 +1,21 @@
-class Game {
-}
+package otaniemipeli
 
+import scala.swing._
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer
+
+/*
+ * This object is the main application.
+ */
+object Game extends SimpleSwingApplication {
+  
+  def top = new MainFrame {
+    
+    title = "Otaniemipeli"
+    size = new Dimension(400, 400)
+    background = Color.black;    
+  }
+}
