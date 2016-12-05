d2c1c8dda33c329cd21f7efc5c793f9fea85b47e
Jonesus <joonas.palosuo@aalto.fi>
2016-12-05 14:59:10 +0200

    Merged new player movement

--- src/uusipeli/Player.scala
+++ src/uusipeli/Player.scala
@@ -7,6 +7,12 @@ class Player {
   
   var position_x = 0.0
   var position_y = 0.0
+  var deltaX = 0
+  var deltaY = 0
+  var xVelocity = 0
+  var yVelocity = 0
+  var maxSpeed = 20
+  var acceleration = 1
   
   var default_speed = 0.8
   
@@ -20,34 +26,51 @@ class Player {
   var image = drawImage()
   
   def turnUp() = {
-    speed_x = 0
-    speed_y = -default_speed
+    deltaY -= acceleration
   }
   
   def turnDown() = {
-    speed_x = 0
-    speed_y = default_speed
+    deltaY += acceleration
   }
   
   def turnLeft() = {
-    speed_x = -default_speed
-    speed_y = 0
+    deltaX -= acceleration
   }
   
   def turnRight() = {
-    speed_x = default_speed
-    speed_y = 0
-  }  
+    deltaX += acceleration
+  }
   
+
   def render: BufferedImage = {
     return image
+  }
+  
+  def move(dx: Int, dy: Int) = {
+    position_x += dx
+    position_y += dy
   }
   
   def update() = {
     
     /* Here we update the player's position. */
-    position_x += speed_x
-    position_y += speed_y
+    if (deltaX == 0 && xVelocity != 0) {
+      deltaX = if (xVelocity > 0) 0 - acceleration else acceleration
+    }
+    if (deltaY == 0 && yVelocity != 0) {
+      deltaY = if (yVelocity > 0) 0 - acceleration else acceleration
+    }
+    
+    if (scala.math.abs(xVelocity + deltaX) < maxSpeed) {
+      xVelocity += deltaX
+    }
+    if (scala.math.abs(yVelocity + deltaY) < maxSpeed) {
+      yVelocity += deltaY
+    }
+    
+    move(xVelocity, yVelocity)
+    deltaX = 0
+    deltaY = 0
   }
   
   def drawImage(): BufferedImage = {
