3548ff66737236d460a52653d2604dae7dbb2f6e
Jonesus <joonas.palosuo@aalto.fi>
2016-12-22 19:17:50 +0200

    Menu changes

--- src/uusipeli/Menu.scala
+++ src/uusipeli/Menu.scala
@@ -1,15 +1,25 @@
 package uusipeli
-package scala.swing
+
+import scala.swing._
+import java.awt.Color
+import uusipeli.model._
+import scala.collection.mutable.Buffer
+
+
+
+
 
 //tarkista importtien tarpeellisuus
 import swing._
 import event._
 import Swing._
 import ListView._
-/*
+
 object Menu extends SimpleSwingApplication {
   def top = new MainFrame{
     title = "Otaniemipeli"
+    size = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT)
+    background = Color.black
     
     menuBar = new MenuBar {
       contents += new Menu("Choose your level"){
@@ -23,11 +33,28 @@ object Menu extends SimpleSwingApplication {
           
         })
         contents += new MenuItem(Action("Exit game"){
-          game.dispose()
+          //game.dispose()
           quit()
         })
       }
     }
+    
+
+    
+    contents = new FlowPanel() {
+      preferredSize = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT)
+      val s = new Dimension(300, 50)
+      contents += new Button("asd") {
+        preferredSize = s
+      }
+      contents += new Button("dsa") {
+        preferredSize = s
+      }
+      contents += new Button("sad") {
+        preferredSize = s
+      }
+    }
+    
+    
   }
 }
-*/
