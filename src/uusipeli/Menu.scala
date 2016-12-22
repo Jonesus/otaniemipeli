fc31c393f652eaa712ac265245ef4c877987697e
Minja <minja.axelsson@aalto.fi>
2016-12-22 15:27:44 +0200

    Game menu added. non-functional

---
+++ src/uusipeli/Menu.scala
@@ -0,0 +1,31 @@
+package uusipeli
+package scala.swing
+
+import swing._
+import event._
+import Swing._
+import ListView._
+
+object Menu extends SimpleSwingApplication {
+  def top = new MainFrame{
+    title = "Otaniemipeli"
+    
+    menuBar = new MenuBar {
+      contents += new Menu("Choose your level"){
+        contents += new MenuItem(Action("JMT"){
+          
+        })
+        contents += new MenuItem(Action("Otakaari"){
+          
+        })
+        contents += new MenuItem(Action("SMT"){
+          
+        })
+        contents += new MenuItem(Action("Exit game"){
+          game.dispose()
+          quit()
+        })
+      }
+    }
+  }
+}
