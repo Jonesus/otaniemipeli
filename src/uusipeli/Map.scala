2ca3bc50f7a227dcb2168ea4c4d36975f3dac92c
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-06 15:55:30 +0200

    Create own class file for Slice.

--- src/uusipeli/Map.scala
+++ src/uusipeli/Map.scala
@@ -6,14 +6,10 @@ import java.awt.Graphics2D
 import javax.imageio.ImageIO
 import java.io.File
 import scala.util.Random
-import uusipeli.items.Olutpullo
 
 
 
 class Map {
-  val rand = new scala.util.Random
-
-  
   var width = 0
   var height = 0
   
@@ -25,33 +21,4 @@ class Map {
   
   var slices = ArrayBuffer[Slice]()
   var items = ArrayBuffer[Item]()
-}
-
-class Slice {
-  val rand = new scala.util.Random
-
-  var index = 0
-  val width = 600
-  val height = 200
-  val items = ArrayBuffer[Item]()
-  
-  val filename = "gfx/slice.png"
-  val background_image = Some(ImageIO.read(new File(filename)))
-  
-  def render: BufferedImage = {
-    new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
-  }
-  
-  def populate = {
-    var i = 0
-    for (i <- 0 to rand.nextInt(1)) {
-      items += new Olutpullo
-      items(i).position_x = 130 + rand.nextInt(340)
-      items(i).position_y = 20 + rand.nextInt(100)
-      items(i).width = 20
-      items(i).height = 64
-      items(i).slice_index = index
-    }
-  }
-  
 }
