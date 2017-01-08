47a67f55319522d13501671e82d65afaf65b34a6
Jonesus <joonas.palosuo@aalto.fi>
2017-01-08 14:57:38 +0200

    Tweaked global constants

--- src/uusipeli/Package.scala
+++ src/uusipeli/Package.scala
@@ -1,9 +1,9 @@
 package uusipeli
 
 package object model {
-  val WINDOW_WIDTH = 1000
-  val WINDOW_HEIGHT = 600
-  val SLICE_HEIGHT = 200
-  val SLICE_WIDTH = WINDOW_WIDTH
+  val WINDOW_WIDTH = 1024
+  val WINDOW_HEIGHT = 700
+  val SLICE_HEIGHT = 128
+  val SLICE_WIDTH = 1024
   val WALL_WIDTH = 100
 }
