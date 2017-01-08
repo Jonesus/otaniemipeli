b76bf51cb93c942127c64b5bceef194ebe541576
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-08 21:41:30 +0200

    Add new items to slices. Implement randomItem() method.

--- src/uusipeli/Slice.scala
+++ src/uusipeli/Slice.scala
@@ -6,7 +6,7 @@ import java.awt.Graphics2D
 import javax.imageio.ImageIO
 import java.io.File
 import scala.util.Random
-import uusipeli.items.Olutpullo
+import uusipeli.items._
 
 class Slice {
   val rand = new scala.util.Random
@@ -24,14 +24,23 @@ class Slice {
   }
   
   def populate = {
-    var i = 0
     for (i <- 0 to rand.nextInt(1)) {
-      items += new Olutpullo
-      items(i).position_x = 130 + rand.nextInt(340)
-      items(i).position_y = 20 + rand.nextInt(100)
-      items(i).width = 20
-      items(i).height = 64
-      items(i).slice_index = index
+      val n: Option[Item] = randomItem()
+      n.foreach { i =>
+        i.position_x = 130 + rand.nextInt(340)
+        i.position_y = 20 + rand.nextInt(100)
+        i.slice_index = index
+        this.items += i 
+      }
     }
+  }
+  
+  def randomItem(): Option[Item] = {
+    val itemType = rand.nextInt(4)
+    if (itemType == 0) return Some(new Olutpullo())
+    if (itemType == 1) return Some(new Noppa())
+    if (itemType == 2) return Some(new Nakki())
+    if (itemType == 3) return Some(new Jaa())
+    None
   }
 }
