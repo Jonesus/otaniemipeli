b5fc28533c47316496e28112e2b1c0529082feaa
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-08 21:40:25 +0200

    Implement sound effect cache and playing.

--- src/uusipeli/ItemStatic.scala
+++ src/uusipeli/ItemStatic.scala
@@ -5,6 +5,7 @@ import java.awt.Graphics2D
 import java.awt.Color
 import javax.imageio.ImageIO
 import java.io.File
+import javax.sound.sampled._
 
 class ItemStatic {
   var imageFilename = ""
@@ -13,6 +14,30 @@ class ItemStatic {
   
   var image: Option[BufferedImage] = None
   
+  var wavClip: Option[Clip] = None
+  
+  /* We load the audio file to memory. */
+  def loadSound(): Unit = {
+    if (wavClip.isDefined) return
+    
+    try {
+      wavClip = Some(AudioSystem.getClip())
+      wavClip.get.open(AudioSystem.getAudioInputStream(new File(soundFilename)))
+    } catch {
+      case e: Exception => println("Could not open sound file " + soundFilename + ": " + e.toString())
+    }
+  }
+  
+  def playSound() = {
+    try {
+      wavClip.foreach { c =>
+        c.setMicrosecondPosition(0)
+        c.start()
+      }
+    } catch {
+      case e: Exception => println("Error: Could not play sound file " + soundFilename + ": " + e.toString())
+    }
+  }
   def getImage(): BufferedImage = {
     if (this.image.isDefined) {
     
