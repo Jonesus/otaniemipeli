1665b77c512e091cc19f3157413ea550b9913084
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-13 16:53:18 +0200

    Implement reset method that repopulates the map.

--- src/uusipeli/levels/LevelOne.scala
+++ src/uusipeli/levels/LevelOne.scala
@@ -17,19 +17,10 @@ class LevelOne extends Map {
   background_image_filename = "gfx/level1_background.png"
   
   /* Background music file. */
-  background_music_filename = "music/level1_music.wav"
+  background_music_filename = "sounds/pahkinansarkija.wav"
   
   val rand = new scala.util.Random
   val bg_files = List("gfx/bg1.png", "gfx/bg2.png", "gfx/bg3.png", "gfx/bg4.png")
-  
-  /* Generate map slices */
-  slices += new Slice("gfx/bg_testi.png")
-  slices += new Slice("gfx/bg2.png")
-  slices += new Slice("gfx/bg3.png")
-  
-  slices(0).index = 0
-  slices(1).index = 1
-  slices(2).index = 2
   
   // Test object
   /*
@@ -40,10 +31,24 @@ class LevelOne extends Map {
   slices(0).items += noppa
 	*/
   
-  for (i <- 3 to 50) {
-    slices += new Slice(bg_files(rand.nextInt(bg_files.length)))
-    slices(i).index = i
-    slices(i).populate
-    items = items ++ slices(i).items
+  def reset() = {
+    /* Generate map slices */
+    slices.clear()
+    slices += new Slice("gfx/bg_testi.png")
+    slices += new Slice("gfx/bg2.png")
+    slices += new Slice("gfx/bg3.png")
+  
+    slices(0).index = 0
+    slices(1).index = 1
+    slices(2).index = 2
+    
+    for (i <- 3 to 50) {
+      slices += new Slice(bg_files(rand.nextInt(bg_files.length)))
+      slices(i).index = i
+      slices(i).populate
+      items = items ++ slices(i).items
+    }
   }
+  
+  reset()
 }
