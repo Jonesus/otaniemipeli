8cb872966d56dd76acf1e367ab194c9da0c5be4c
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-13 16:07:40 +0200

    Use window width as world width.

--- src/uusipeli/levels/LevelOne.scala
+++ src/uusipeli/levels/LevelOne.scala
@@ -2,15 +2,16 @@ package uusipeli.levels
 
 import uusipeli.Map
 import uusipeli.Slice
+import uusipeli.model._
 
 class LevelOne extends Map {
   /* Dimensions of this level. */
-  width = 1200
+  width = WINDOW_WIDTH
   height = 1200
   
   /* Player's starting position. */
-  player_position_x = 300
-  player_position_y = 300
+  player_position_x = width / 2
+  player_position_y = 200
   
   /* Background image file. */
   background_image_filename = "gfx/level1_background.png"
@@ -22,9 +23,22 @@ class LevelOne extends Map {
   val bg_files = List("gfx/bg1.png", "gfx/bg2.png", "gfx/bg3.png", "gfx/bg4.png")
   
   /* Generate map slices */
-  slices += new Slice("gfx/bg1.png")
+  slices += new Slice("gfx/bg_testi.png")
   slices += new Slice("gfx/bg2.png")
   slices += new Slice("gfx/bg3.png")
+  
+  slices(0).index = 0
+  slices(1).index = 1
+  slices(2).index = 2
+  
+  // Test object
+  /*
+  var noppa = new uusipeli.items.Noppa()
+  noppa.position_x = 0
+  noppa.position_y = 0
+
+  slices(0).items += noppa
+	*/
   
   for (i <- 3 to 50) {
     slices += new Slice(bg_files(rand.nextInt(bg_files.length)))
