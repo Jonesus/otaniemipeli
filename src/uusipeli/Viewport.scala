f189a00267b849e753b2af1992220ed824ef38de
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-13 16:01:56 +0200

    Fix coordinate transformations in rendering. Implement listener for pause key.

--- src/uusipeli/Viewport.scala
+++ src/uusipeli/Viewport.scala
@@ -5,16 +5,24 @@ import scala.swing.event._
 import java.awt.event._
 import java.awt.image.BufferedImage
 import java.awt.Color
+import uusipeli.model._
+
+/*
+ * Viewport_x and viewport_y: The middle of the viewport in world coordinates.
+ */
 
 class Viewport(world: World, viewport_width: Int, viewport_height: Int, var viewport_x: Int, var viewport_y: Int) extends Panel {
+  // Background color the the viewport.
   this.background = Color.black
   
   val viewport_image = new BufferedImage(viewport_width, viewport_height, BufferedImage.TYPE_INT_ARGB)
   
+  // We set this component focusable and request focus, so we can react to keyboard events.
   focusable = true
   
   requestFocus()
   
+  // Listen to keyboard events.
   listenTo(keys)
   
   reactions += {
@@ -22,50 +30,92 @@ class Viewport(world: World, viewport_width: Int, viewport_height: Int, var view
     case KeyPressed(_, Key.S, _, _) => Game.keyPressed("s")
     case KeyPressed(_, Key.A, _, _) => Game.keyPressed("a")
     case KeyPressed(_, Key.D, _, _) => Game.keyPressed("d")
+    
     case KeyReleased(_, Key.W, _, _) => Game.keyReleased("w")
     case KeyReleased(_, Key.S, _, _) => Game.keyReleased("s")
     case KeyReleased(_, Key.A, _, _) => Game.keyReleased("a")
     case KeyReleased(_, Key.D, _, _) => Game.keyReleased("d")
+    
+    case KeyTyped(_, 'P', _, _) => Game.pauseKeyPressed()
+    case KeyTyped(_, 'p', _, _) => Game.pauseKeyPressed()
   }
   
   def render() = {
     val viewport_graphics = viewport_image.getGraphics
     
+    val viewportUpperLeftXInWorldCoordinates = viewport_x - (viewport_width / 2)
+    val viewportUpperLeftYInWorldCoordinates = viewport_y - (viewport_height / 2)
+    
     /* Clear the buffer. */
     viewport_graphics.clearRect(0, 0, viewport_width, viewport_height)
     
     
-    /* Draw items from slices. */
+    /* Draw slices and their items. */
     for (slice <- world.slices) {
-      // Is this object visible?
+      
+      /* Is this slice visible? */
       if (
-        slice.index * slice.height > (viewport_y - viewport_height)
-        && slice.index * slice.height < (viewport_y + viewport_height)) {
-            
+        (slice.index * slice.height) > (viewport_y - (viewport_height / 2))
+        || (slice.index * slice.height) < (viewport_y + (viewport_height / 2))) {
+        
+        /* Draw the background of the slice.
+         * 
+         * Transform world coordinates into viewport coordinates.
+         */
         viewport_graphics.drawImage(
             slice.background_image.get,
             0,
-            slice.index * slice.height - viewport_y,
+            slice.index * slice.height - (viewport_y - (viewport_height / 2)),
             null)
         
+        /* Draw all visible items in this slice.
+         * 
+         * Slice coordinates -> world coordinates -> viewport coordinates.
+         */
+               
+        // Item coordinates in world coordinate space.
+        var itemXWorld, itemYWorld = 0
+
+        // Item coordinates (the middle point of the item) in viewport coordinate space.
+        // (0, 0) is the upper left corner of the viewport.
+        var itemXViewport, itemYViewport = 0
+        
         for (item <- slice.items) {
-          if (item.active == true) {
+          if (item.visible == true) {
+          
+          // We assume that the slice is as wide as the world.
+          itemXWorld = item.position_x
+          itemYWorld = slice.index * SLICE_HEIGHT + item.position_y
+          
+          itemXViewport = itemXWorld 
+          itemYViewport = itemYWorld - viewportUpperLeftYInWorldCoordinates
+          
+          // println("Rendered item. Local coordinates: (" + item.position_x + ", " + item.position_y + ") World coordinates: (" + itemXWorld + ", " + itemYWorld + ")")
+
             viewport_graphics.drawImage(
                 item.render,
-                item.position_x - (item.width / 2),
-                slice.index * slice.height + (item.position_y - (item.height / 2)) - viewport_y,
+                itemXViewport - (item.width / 2),
+                itemYViewport - (item.height / 2),
                 null)
           }
         }
       }
     }
     
+    /*
+     * Player's coordinates (middle point of the player) in viewport coordinates, where (0, 0) is the upper left corner.
+     */
+    val playerXViewport = world.getPlayer.position_x.toInt - viewportUpperLeftXInWorldCoordinates
+    val playerYViewport = world.getPlayer.position_y.toInt - viewportUpperLeftYInWorldCoordinates
+    
     /* Draw player on top of the image. */
     viewport_graphics.drawImage(
             world.getPlayer.render,
-            world.getPlayer.position_x.toInt,
-            world.getPlayer.position_y.toInt - viewport_y,
+            playerXViewport - (world.getPlayer.width / 2),
+            playerYViewport - (world.getPlayer.height / 2),
             null)
+            
+    /* TODO: Draw the level name. */
     
   }
 
@@ -79,6 +129,6 @@ class Viewport(world: World, viewport_width: Int, viewport_height: Int, var view
   }
   
   def update() = {
-    viewport_y += 3  // Scroll screen downwards
+    viewport_y += 2  // Scroll screen downwards
   }
 }
