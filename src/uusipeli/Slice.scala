5395ff207a58fb8d143014b10dbbcf3839a97e25
Emilia Paltta <palttaemilia@gmail.com>
2017-01-13 22:14:59 +0200

    Items Prujut and Spagetti added in Slice.scala

--- src/uusipeli/Slice.scala
+++ src/uusipeli/Slice.scala
@@ -37,12 +37,14 @@ class Slice(filename: String) {
   }
   
   def randomItem(): Option[Item] = {
-    val itemType = rand.nextInt(5)
+    val itemType = rand.nextInt(7)
     if (itemType == 0) return Some(new Olutpullo())
     if (itemType == 1) return Some(new Noppa())
     if (itemType == 2) return Some(new Nakki())
     if (itemType == 3) return Some(new Jaa())
     if (itemType == 4) return Some(new Kivi())
+    if (itemType == 5) return Some(new Prujut())
+    if (itemType == 6) return Some(new Spagetti())
     None
   }
 }
