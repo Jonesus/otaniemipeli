0e504354c23999568fb4e6f3a1caf7c94f9d3146
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-14 16:46:56 +0200

    Add level name item.

--- src/uusipeli/levels/LevelOne.scala
+++ src/uusipeli/levels/LevelOne.scala
@@ -3,6 +3,7 @@ package uusipeli.levels
 import uusipeli.Map
 import uusipeli.Slice
 import uusipeli.model._
+import uusipeli.items._
 
 class LevelOne extends Map {
   /* Dimensions of this level. */
@@ -22,15 +23,6 @@ class LevelOne extends Map {
   val rand = new scala.util.Random
   val bg_files = List("gfx/bg1.png", "gfx/bg2.png", "gfx/bg3.png", "gfx/bg4.png")
   
-  // Test object
-  /*
-  var noppa = new uusipeli.items.Noppa()
-  noppa.position_x = 0
-  noppa.position_y = 0
-
-  slices(0).items += noppa
-	*/
-  
   def reset() = {
     /* Generate map slices */
     slices.clear()
@@ -43,6 +35,10 @@ class LevelOne extends Map {
     slices(0).index = 0
     slices(1).index = 1
     slices(2).index = 2
+    
+    /* Add level name item to the third slice. */
+    slices(2).items += new LevelNameItem("gfx/title jmt.png")
+    slices(2).items(0).position_x = WINDOW_WIDTH / 2
     
     for (i <- 3 to 50) {
       slices += new Slice(bg_files(rand.nextInt(bg_files.length)))
