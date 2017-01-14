f776dfa389dce8ffe22561926b6ad21ab0fe0421
Jonesus <joonas.palosuo@aalto.fi>
2017-01-14 21:29:53 +0200

    Removed old stuff, added new buttons

--- src/uusipeli/Main.scala
+++ src/uusipeli/Main.scala
@@ -25,43 +25,12 @@ object Main extends SimpleSwingApplication {
     size = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT)
     background = Color.black
     
-    menuBar = new MenuBar {
-      contents += new Menu("Menu"){
-        contents += new MenuItem(Action("Go to menu"){
-          go_to_menu()
-        })
-        contents += new MenuItem(Action("Exit game"){
-          quit()
-        })
-      }
-    }
     
-    
-    def go_to_menu2() {
+    def go_to_menu() {
       mygame.stopGame
       this.contents = mymenu
     }
     
-    def go_to_menu() {
-      mygame.stopGame
-      this.contents = new FlowPanel() {
-        preferredSize = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT)
-        val s = new Dimension(300, 50)
-        contents += new Button(Action("JMT"){
-          start_game(new LevelOne)
-        }) {
-          preferredSize = s
-        }
-        contents += new Button(Action("SMT"){
-          start_game(new LevelTwo)
-        }) {
-          preferredSize = s
-        }
-        contents += new Button("existence is pain") {
-          preferredSize = s
-        }
-      }
-    }
     
     def start_game(lvl: Map) {
       this.contents = mygame.viewport
@@ -69,6 +38,15 @@ object Main extends SimpleSwingApplication {
       mygame.startGame(lvl)
     }
     
-    go_to_menu2()
+    def startJMT(x: Unit) { start_game(new LevelOne) }
+    def startSMT(x: Unit) { start_game(new LevelTwo) }
+    def startOTA(x: Unit) { start_game(new LevelTwo) }
+    
+    mymenu.JMTbutton.action = startJMT 
+    mymenu.SMTbutton.action = startSMT
+    mymenu.OTAbutton.action = startOTA
+
+
+    go_to_menu()
   }
 }
