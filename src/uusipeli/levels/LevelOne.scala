a772a439eff8aab9743a63b3a7a270bfa5290780
Jonesus <joonas.palosuo@aalto.fi>
2017-01-14 14:59:57 +0200

    Removed commented code

--- src/uusipeli/levels/LevelOne.scala
+++ src/uusipeli/levels/LevelOne.scala
@@ -13,23 +13,12 @@ class LevelOne extends Map {
   player_position_x = width / 2
   player_position_y = 200
   
-  /* Background image file. */
   background_image_filename = "gfx/level1_background.png"
-  
-  /* Background music file. */
   background_music_filename = "sounds/pahkinansarkija.wav"
   
   val rand = new scala.util.Random
   val bg_files = List("gfx/bg1.png", "gfx/bg2.png", "gfx/bg3.png", "gfx/bg4.png")
-  
-  // Test object
-  /*
-  var noppa = new uusipeli.items.Noppa()
-  noppa.position_x = 0
-  noppa.position_y = 0
 
-  slices(0).items += noppa
-	*/
   
   def reset() = {
     /* Generate map slices */
