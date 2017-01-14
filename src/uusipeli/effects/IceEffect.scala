781943b5b62f4645ec157c5bdaee62576597ad83
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-14 13:24:02 +0200

    Get player’s speed from Package.scala.

--- src/uusipeli/effects/IceEffect.scala
+++ src/uusipeli/effects/IceEffect.scala
@@ -2,6 +2,7 @@ package uusipeli.effects
 
 import uusipeli.Effect
 import uusipeli.Game
+import uusipeli.model._
 
 class IceEffect extends Effect {
   
@@ -9,10 +10,10 @@ class IceEffect extends Effect {
   val ratio = 4
   
   override def start() = {
-    Game.player.Y_RESTING_SPEED = Game.player.Y_RESTING_SPEED * ratio
+    Game.player.Y_RESTING_SPEED = PLAYER_SPEED_DOWN * ratio
   }
   
   override def end() = {
-    Game.player.Y_RESTING_SPEED = Game.player.Y_RESTING_SPEED / ratio
+    Game.player.Y_RESTING_SPEED = PLAYER_SPEED_DOWN
   }
 }
