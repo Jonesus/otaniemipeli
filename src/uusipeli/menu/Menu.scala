9287aaf442cb8c417afa971fc742fd2dd91941b2
Jonesus <joonas.palosuo@aalto.fi>
2017-01-14 21:30:25 +0200

    New visual menu

--- src/uusipeli/menu/Menu.scala
+++ src/uusipeli/menu/Menu.scala
@@ -4,6 +4,7 @@ import scala.swing._
 import java.awt.Color
 import uusipeli.model._
 import scala.collection.mutable.Buffer
+import scala.collection.mutable.ArrayBuffer
 import java.awt.image.BufferedImage
 import uusipeli.levels._
 
@@ -13,15 +14,25 @@ import java.io.File
 
 import java.awt.geom._
 
-
-//tarkista importtien tarpeellisuus
-import swing._
-import event._
-import Swing._
-import ListView._
+import scala.swing.event._
+import java.awt.event._
 
 
 class MenuView extends Panel {
+  
+  focusable = true
+  listenTo(mouse.clicks)
+  
+  reactions += {
+      case e: MouseClicked => {
+        println("Mouse clicked at " + e.point)
+        for (button <- buttons) {
+          button.isClicked(e.point)
+        }
+      }
+    }
+  
+  
   
   val fontFile = new File("gfx/Gamer.ttf")
   val new_font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
@@ -31,20 +42,32 @@ class MenuView extends Panel {
   
   this.background = Color.darkGray
   this.preferredSize = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT)
-
   val menu_image = new BufferedImage(WINDOW_WIDTH, WINDOW_HEIGHT, BufferedImage.TYPE_INT_ARGB)
-
-  var rec = new RoundRectangle2D.Double(100, 100, 400, 200, 50, 50)
+  
+  
+  var JMTbutton = new visualButton(0, 200, "gfx/title jmt.png", null)
+  var SMTbutton = new visualButton(0, 300, "gfx/title smt.png", null)
+  var OTAbutton = new visualButton(0, 400, "gfx/title otakaari.png", null)
+  var buttons = new ArrayBuffer[visualButton]()
+  buttons += JMTbutton
+  buttons += SMTbutton
+  buttons += OTAbutton
   
   
   def render(g: Graphics2D) {
     g.setColor(new Color(255, 255, 255))
-    g.setFont(new Font("Gamer", Font.PLAIN, 60))
-    g.drawString("vittu JEEE", 300, 300)
+    g.setFont(new Font("Gamer", Font.PLAIN, 100))
+    g.drawString("TEEKKARISIMULAATTORI", 130, 80)
     
-    g.setColor(Color.CYAN)
-    g.fill(rec)
-
+    
+    var menu_graphics = menu_image.getGraphics()
+    
+    for (button <- buttons) {
+      menu_graphics.drawImage(button.render(),
+                              button.x,
+                              button.y,
+                              null)
+    }
     
   }
   
