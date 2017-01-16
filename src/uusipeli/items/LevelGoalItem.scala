bcd3157abd1e4217d1d78330612ac4e56fce417e
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-17 01:58:18 +0200

    Send an EndGameEvent when this item is touched.

--- src/uusipeli/items/LevelGoalItem.scala
+++ src/uusipeli/items/LevelGoalItem.scala
@@ -3,13 +3,15 @@ package uusipeli.items
 import uusipeli.Item
 import uusipeli.ItemStatic
 import uusipeli.Game
+import uusipeli.Event
+import uusipeli.events.EndGameEvent
 
 class LevelGoalItem(val filename: String) extends Item {
   LevelGoalItem.imageFilename = filename
   this.setImage(LevelGoalItem.getImage())
   
   override def processCollision() = {
-    Game.stopGame()
+    Game.addEvent(new EndGameEvent())
   }
 }
 
