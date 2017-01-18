a12a81adc4c07d0b4c8149e6f37468496a1e22c3
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-18 14:30:13 +0200

    Rename timeout -> delay. Add variable started.

--- src/uusipeli/Effect.scala
+++ src/uusipeli/Effect.scala
@@ -1,15 +1,17 @@
 package uusipeli
 
+/* This class represents effects, such as BeerEffect, that affect the game's state for a while. */
+/* Effects are processed in the game loop. */
 abstract class Effect {
-  //Define abstract class for Effect for separate effects to refer to.
   var startTime: Long = 0
-  var timeout = 0
+  var delay = 0
+  var started = false
   
   def start() = {
-    
+    /* Code to run when the effect is added to the queue. */
   }
   
   def end() = {
-    
+    /* Code to run when delay has passed. */
   }
 }
