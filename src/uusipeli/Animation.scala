1057267db61ac702716f4dbda17dbbe3c54ff4b9
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-18 13:08:57 +0200

    Refactoring and commenting.

--- src/uusipeli/Animation.scala
+++ src/uusipeli/Animation.scala
@@ -5,32 +5,47 @@ import scala.collection.mutable.ArrayBuffer
 import java.io.File
 import javax.imageio.ImageIO
 
+/* This class represents an animation that is build from BufferedImage frames. */
 class Animation {
-  var frameDuration = 400
-  val frames = ArrayBuffer[BufferedImage]()
-  var width = 0
-  var height = 0
-  var lastRenderTime: Long = 0
-  var currFrame = 0
   
+  /* The default duration of a frame, in milliseconds. */
+  var frameDuration = 400
+  private val frames = ArrayBuffer[BufferedImage]()
+  
+  private var width = 0
+  private var height = 0
+  
+  /* When was the frame changed? */
+  private var lastRenderTime: Long = 0
+  private var currFrame = 0
+  
+  /* Adds a new frame to the list.
+   * 
+   * fileName: The file name of the frame to be added.
+   */
   def addFrame(fileName: String) = {
     try {
       val image = ImageIO.read(new File(fileName))
-      width = image.getWidth()
-      height = image.getHeight()
+      this.width = image.getWidth()
+      this.height = image.getHeight()
       this.frames += image
     } catch {
       case e: Exception => println("Could not load animation frame from file " + fileName + ": " + e.toString())
     }
   }
   
+  /* Renders the current frame. If frameDuration has passed since the last render,
+   * changes the image.
+   */
   def render(): BufferedImage = {
-    if (frames.size == 0) return new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB)
+    /* If the animation is empty, returns a 1 pixel x 1 pixel empty image. */
+    if (this.frames.size == 0) return new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB)
     
     val currTime = scala.compat.Platform.currentTime
     if (currTime > (this.lastRenderTime + this.frameDuration)) {
-      //Changes the frame of the animation if sufficient time has passed.
+      /* Changes the frame. */
       if (this.currFrame == (this.frames.size - 1)) this.currFrame = 0 else this.currFrame += 1
+      
       this.lastRenderTime = scala.compat.Platform.currentTime
     }
     return this.frames(this.currFrame)
