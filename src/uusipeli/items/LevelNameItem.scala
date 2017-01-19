7f4b20df3a124a0c4b419b037cad82b688a891b4
Juhana Saarinen <juhana.saarinen@aalto.fi>
2017-01-19 19:14:25 +0200

    Fix image loading for LevelNameItem: Call loadImage() every time the item is created.

--- src/uusipeli/items/LevelNameItem.scala
+++ src/uusipeli/items/LevelNameItem.scala
@@ -5,6 +5,7 @@ import uusipeli.ItemStatic
 
 class LevelNameItem(val filename: String) extends Item {
   LevelNameItem.imageFilename = filename
+  LevelNameItem.loadImage()
   this.setImage(LevelNameItem.getImage())
   this.active = false
 }
